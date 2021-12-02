132c35a43c2b4eedaa3726d1fe8a3183fa29227b
Tiago Lira <tiago.lira.p@gmail.com>
2021-12-02 16:02:49 +0000

    Updated video link

--- app/src/main/java/com/syncedapps/inthegametvexample/MovieList.kt
+++ app/src/main/java/com/syncedapps/inthegametvexample/MovieList.kt
@@ -17,7 +17,7 @@ object MovieList {
         val studio = arrayOf(
                 "Inthegame")
         val videoUrl = arrayOf(
-            "https://media.inthegame.io/uploads/videos/streamers/a64706dd0f42356e93d299075940c456.857ecbb7a131f9bb4940a6b8ad5ec70e.mp4")
+            "https://media2.inthegame.io/uploads/videos/streamers/a64706dd0f42356e93d299075940c456.857ecbb7a131f9bb4940a6b8ad5ec70e.mp4")
         val bgImageUrl = arrayOf(
                 "ball")
         val cardImageUrl = arrayOf(
