08ad2441f37319a665b245117bb4adc2e448a609
roman.havdulskyi <romeogavdulskiy@gmail.com>
2025-09-15 17:01:53 +0300

    Updated accountId and channelSlug

--- app/src/main/java/com/syncedapps/inthegametvexample/PlaybackVideoFragment.kt
+++ app/src/main/java/com/syncedapps/inthegametvexample/PlaybackVideoFragment.kt
@@ -43,8 +43,8 @@ class PlaybackVideoFragment : VideoSupportFragment()  {
         play()
 
         // Replace 'your_account_id' and 'your_channel_slug' with actual values
-        val accountId = "68b6f55cf49cd69a3dee4a05"
-        val channelSlug = "redgetest"
+        val accountId = "68650da0324217d506bcc2d4"
+        val channelSlug = "samplechannel"
 
 
         // Initialize ITGPlaybackComponent
