7cb7a74f2e9c2c02965c17c3edc57ee7079c5957
roman.havdulskyi <romeogavdulskiy@gmail.com>
2025-11-20 17:41:50 +0200

    ITG: updated to 2.6.37

--- app/src/main/java/com/syncedapps/inthegametvexample/Const.kt
+++ app/src/main/java/com/syncedapps/inthegametvexample/Const.kt
@@ -2,5 +2,5 @@ package com.syncedapps.inthegametvexample
 
 
 object Const {
-    const val VIDEO_URL = "https://media.inthegame.io/uploads/videos/streamers/e5ba6651e74cd163d9159b4284581c54.6ba8db5ef482491e79b7371146a413b8.mp4"
+    const val VIDEO_URL = "https://assets.inthegame.io/admin-assets/black_screen_with_timer.mp4"
 }
